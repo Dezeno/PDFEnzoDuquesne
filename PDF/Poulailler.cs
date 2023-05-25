bc8e2bad2faeba8787843f23d71d8146b8033dde
Enzo Duquesne <enzo.duquesne@etud.u-picardie.fr>
2023-05-25 09:25:30 +0200

    Ajoutez des fichiers projet.

---
+++ PDF/Poulailler.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF
+{
+    /// <summary>
+    /// Modélise un poulailler
+    /// </summary>
+    public class Poulailler
+    {
+        /// <summary>
+        /// Nom du poulailler
+        /// </summary>
+        string nom;
+
+        /// <summary>
+        /// Capacité maximale du poulailler (nombre de poules)
+        /// </summary>
+        int capaciteMax;
+
+        /// <summary>
+        /// Liste des poules présentes dans le poulailler
+        /// </summary>
+        List<Poule> listePoules = new List<Poule>();
+
+        /// <summary>
+        /// Création d'une nouvelle instance d'un objet Poulailler
+        /// </summary>
+        /// <param name="nomPoulailler">Nom</param>
+        /// <param name="capacitePoulailler">Nombre de poules maximum dans un poulailler</param>
+        public Poulailler(string nomPoulailler, int capacitePoulailler)
+        {
+            nom = nomPoulailler;
+            capaciteMax = capacitePoulailler;
+        }
+
+        /// <summary>
+        /// Ajoute une poule au poulailler
+        /// </summary>
+        /// <param name="p1">Poule à ajouter</param>
+        public void AjoutPoule(Poule p1)
+        {
+            if(listePoules.Count == capaciteMax)
+            {
+                Console.WriteLine($"Le poulailler {nom} est plein, essayez en un autre ou " +
+                    $"créez en un nouveau.");
+            }
+            else
+            {
+                listePoules.Add( p1 );
+            }
+        }
+
+        /// <summary>
+        /// Supprime une poule au poulailler
+        /// </summary>
+        /// <param name="nb">Numéro de la poule à supprimer</param>
+        public void SupprimerPoule(int nb)
+        {
+            listePoules.RemoveAt(nb - 1);
+        }
+
+        public void Vider()
+        {
+            listePoules.Clear();
+        }
+
+        /// <summary>
+        /// Affiche les poules du poulailler ainsi que le nombre de poules
+        /// </summary>
+        public void Afficher()
+        {
+            for (int i=0; i<listePoules.Count; i++)
+            {
+                Console.Write($"Poule N°{i+1} : "); listePoules[i].Afficher();
+            }
+            if(listePoules.Count == 0)
+            {
+                Console.WriteLine($"Le poulailler est vide !");
+            }
+            Console.WriteLine($"\nNombre de poules : {listePoules.Count}\n");
+        }
+    }
+}
---
+++ PDF/Program.cs
@@ -0,0 +1,31 @@
+﻿using PDF;
+
+// Instanciation d'un poule Ginger
+Poule ginger = new Poule("Ginger", "gauloise", 0.8, Taille.L);
+Poule fanny = new Poule("Fanny", "sussex", 0.91, Taille.XL);
+
+// Affichage de cette poule
+ginger.Afficher();
+
+// Instanciation d'un poulailler Poulailler1
+Poulailler poulailler1 = new Poulailler("Poulailler 1", 3);
+
+// Ajout de poules dans le poulailler 1
+poulailler1.AjoutPoule(ginger);
+poulailler1.AjoutPoule(fanny);
+poulailler1.AjoutPoule(ginger);
+
+// Affichage du poulailler
+poulailler1.Afficher();
+
+// Suppression de la poule n°3, Ginger (doublon)
+poulailler1.SupprimerPoule(3);
+
+// Affichage du poulailler
+poulailler1.Afficher();
+
+// Vidage du poulailler
+poulailler1.Vider();
+
+// Affichage du poulailler
+poulailler1.Afficher();
